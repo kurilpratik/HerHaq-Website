93ca2b7c5a95aa1447b444e1f5e3efeebd8213b1
Pratik Kuril <pratikkuril16@gmail.com>
2024-11-29 01:49:25 +0530

    Add a flexible navbar component

---
+++ src/data/navbarData.js
@@ -0,0 +1,19 @@
+const navbarData = [
+    {
+        name: "Our Work",
+        path: "/work/"
+    },
+    {
+        name: "Partnerships",
+        path: "/partnerships/"
+    },
+    {
+        name: "Our Team",
+        path: "/team/"
+    },
+    {
+        name: "Blog",
+        path: "/blog/"
+    }
+]
+export default navbarData;
