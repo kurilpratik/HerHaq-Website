90f6040f562899dc39d6cab6d057a25aabc0f621
Pratik Kuril <pratikkuril16@gmail.com>
2025-10-29 03:30:05 +0530

    Add the team page

---
+++ src/data/teamData.js
@@ -0,0 +1,38 @@
+const teamData = {
+  trustees: [
+    {
+      name: "AANYA WIG",
+      title: "CO-FOUNDER",
+      imageUrl: "/assets/team/Aanya Wig.jpg",
+    },
+    {
+      name: "SOUMYA SINGHAL",
+      title: "CO-FOUNDER",
+      imageUrl: "/assets/team/Soumya Singhal_.jpg",
+    },
+    {
+      name: "YAGNESH",
+      title: "MEMBER",
+      imageUrl: "/assets/team/Yagnesh Sharma_.jpg",
+    },
+  ],
+  coreTeam: [
+    {
+      name: "ANVIATA PLAWAT",
+      title: "HEAD OF COMMUNICATIONS",
+      imageUrl: "/assets/team/Arshia.jpg",
+    },
+    {
+      name: "RIHAN SHAREEF",
+      title: "HEAD OF POLICY",
+      imageUrl: "/assets/team/Deepanshi.png",
+    },
+    {
+      name: "SHITIZ PATNAIK",
+      title: "HEAD OF PARTNERSHIPS",
+      imageUrl: "/assets/team/Mehak Saini.png",
+    },
+  ],
+};
+
+export default teamData;
